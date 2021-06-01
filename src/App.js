027fdea9168c9c21de5e9e216f559f4a4897630f
Caitlyn <caitlynmiley.cm@gmail.com>
2021-06-01 12:18:09 -0500

    added template

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-
-import './App.css';
-import Home from "./Home"
-import Navbar from "./Navbar"
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
-
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-        <Router>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Router>
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Testimonials from  './components/Testimonials';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import resumeData from './resumeData';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <Testimonials resumeData={resumeData}/>
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
