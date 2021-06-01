b8b91d53ea4e480b8386fb09a522166ba4fc9e1c
Caitlyn <caitlynmiley.cm@gmail.com>
2021-06-01 16:58:07 -0500

    added depoloyed links to images

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -15,10 +15,9 @@ export default class Porfolio extends Component {
               return(
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
-                    <a href="#modal-01">
-                     
-                
+                    <a href={`${item.link}`} target="_blank">
                       <img src={`${item.imgurl}`} className="item-img"/>
+                   
                       <div className="overlay">
                         <div className="portfolio-item-meta">
                           <h5>{item.name}</h5>
